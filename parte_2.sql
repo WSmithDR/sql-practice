5eefcaaeb2ed15b995ceb586e3c36a7676312e26
WSmithDR <wsduenas@espol.edu.ec>
2025-08-17 21:27:25 -0500

    created

--- parte_2.sql
+++ parte_2.sql
@@ -1,18 +1,20 @@
 use classicmodels;
 
+-- DROP PROCEDURE aumentarCreditoClientes;
+
 DELIMITER //
 
-CREATE PROCEDURE aumentarCreditoClientes(pais VARCHAR(50))
+CREATE PROCEDURE aumentarCreditoClientes(in pais VARCHAR(50))
 BEGIN
     UPDATE customers c
     JOIN (
         SELECT DISTINCT c.customerNumber
         FROM customers c
         JOIN employees e ON c.salesRepEmployeeNumber = e.employeeNumber
-        JOIN offices o ON e.officeCode = o.officeCode
+        JOIN offices o USING(officeCode)
         WHERE o.country = pais
         AND c.salesRepEmployeeNumber IS NOT NULL
-    ) AS target_customers ON c.customerNumber = target_customers.customerNumber
+    ) AS target_customers USING (customerNumber)
     SET c.creditLimit = c.creditLimit * 1.10;
     
    
